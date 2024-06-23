0319c56cd3bd595f2bc69dd75f17f614e59ab1cf
BishtJatin <109663578+BishtJatin@users.noreply.github.com>
2024-06-23 16:29:54 +0530

    main container add

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import Header from './Header'
+import Header from "./Header";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
+
 
 const Browse = () => {
-  return (
-    <div>
-      <Header/>
-    </div>
-  )
-}
+useNowPlayingMovies();
+  
 
-export default Browse
+  return (
+    <div >
+      <Header />
+    
+     <MainContainer />
+  
+          
+          <SecondaryContainer />
+    </div>
+  );
+};
+export default Browse;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -46,11 +46,11 @@ const Header = () => {
 
   return (
     <div className='absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between'>
-        <img className='w-44 mx-auto md:mx-0' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'/>
+        <img className='w-44 mx-auto md:mx-0 ' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'/>
 
-       {user && (<div>
+       {user && (<div className='flex flex-row'>
         <img
-            className="hidden md:block w-12 h-12"
+            className="hidden md:block w-12 h-12 mt-4 mr-5"
             alt="usericon"
             src={user?.photoURL}
           />
